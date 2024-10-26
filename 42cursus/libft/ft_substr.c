90560d9d0585636bfe56e693e92d380916cef4b5
si-park <si-park@c2r10s6.42gyeongsan.kr>
2024-10-26 16:24:35 +0900

    solve substr

--- 42cursus/libft/ft_substr.c
+++ 42cursus/libft/ft_substr.c
@@ -6,13 +6,34 @@
 /*   By: si-park <si-park@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 21:41:27 by si-park           #+#    #+#             */
-/*   Updated: 2024/10/13 19:33:42 by si-park          ###   ########.fr       */
+/*   Updated: 2024/10/26 16:21:08 by si-park          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    
+	char			*cc;
+	unsigned int	i;
+	unsigned int	s_len;
+
+	i = 0;
+	s_len = ft_strlen(s);
+	cc = malloc(len + 1);
+	if (s == NULL)
+		return (NULL);
+	if (start > s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	if (cc == NULL)
+		return (NULL);
+	while (i < len && s[start + i])
+	{
+		cc[i] = s[start + i];
+		i++;
+	}
+	cc[i] = '\0';
+	return (cc);
 }
