81752f0d6e25ee9ed1f5ad65f1e20e7b6e5c46cf
Be4hape <qkrtlgud95@naver.com>
2024-11-08 19:40:38 +0900

    almost solve

--- 42cursus/libft/ft_split.c
+++ 42cursus/libft/ft_split.c
@@ -52,21 +52,17 @@ char **ft_split(char const *s, char c)
     int i = 0;
     char const *start;
     
-    //null의 공간 + 1
     char **result = (char **)malloc(sizeof(char*) * (len_s + 1));
 
     if(result == NULL)
         return NULL;
-
     while(*s)
     {
         if(*s == c)
             s++;
-        //구분자 다음 시작 주소 저장
         if(*s){
             start = s;
             length = 0;
-            //단어 길이 저장
             while(*s && *s != c)
             {
                 length++;
@@ -77,6 +73,7 @@ char **ft_split(char const *s, char c)
         i++;
     }
     result[i] = NULL;
+    return result;
 }
 
 // 결과를 출력하고 메모리를 해제하는 함수
@@ -95,21 +92,37 @@ void print_split_result(char **result) {
     free(result);  // 배열 메모리 해제
 }
 
+int main() {
+    // 테스트할 문자열과 구분자 설정
+    char str1[] = "hello,world,this,is,a,test";
+    char delimiter1 = ',';
 
-int main() 
-{
-    char str[] = "hello,world,this,is,a,test";
-    char delimiter = ',';
+    char str2[] = "42  school    test  code";
+    char delimiter2 = ' ';
 
-    printf("Original string: \"%s\"\n", str);
-    printf("Delimiter: '%c'\n", delimiter);
+    char str3[] = ",hello,,world,,";
+    char delimiter3 = ',';
 
-    // ft_split 호출
-    char **result = ft_split(str, delimiter);
-
-    // 결과 출력
+    // 첫 번째 테스트
+    printf("Original string: \"%s\"\n", str1);
+    printf("Delimiter: '%c'\n", delimiter1);
+    char **result1 = ft_split(str1, delimiter1);
     printf("Split result:\n");
-    print_split_result(result);
+    print_split_result(result1);
+
+    // 두 번째 테스트
+    printf("\nOriginal string: \"%s\"\n", str2);
+    printf("Delimiter: '%c'\n", delimiter2);
+    char **result2 = ft_split(str2, delimiter2);
+    printf("Split result:\n");
+    print_split_result(result2);
+
+    // 세 번째 테스트
+    printf("\nOriginal string: \"%s\"\n", str3);
+    printf("Delimiter: '%c'\n", delimiter3);
+    char **result3 = ft_split(str3, delimiter3);
+    printf("Split result:\n");
+    print_split_result(result3);
 
     return 0;
 }
