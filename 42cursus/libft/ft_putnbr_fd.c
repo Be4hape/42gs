9489d12b45588bf36fff7d3c0f0f9cb310cf61c5
si-park <qkrtlgud95@naver.com>
2024-11-09 23:18:52 +0900

    all solve, need modifie for wrong problems

---
+++ 42cursus/libft/ft_putnbr_fd.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: si-park <si-park@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 22:49:24 by si-park           #+#    #+#             */
+/*   Updated: 2024/11/09 23:15:50 by si-park          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putchar(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr(int nb, int fd)
+{
+	if (nb == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-', fd);
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10, fd);
+		ft_putnbr(nb % 10, fd);
+	}
+	else
+	{
+		ft_putchar(nb + '0', fd);
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	ft_putnbr(n, fd);
+}
