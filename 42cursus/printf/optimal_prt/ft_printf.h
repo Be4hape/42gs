a829de759f86f9da7d5104f37729800c26adc3ee
si-park <qkrtlgud95@naver.com>
2025-02-19 18:23:26 +0900

    modify to eval, makefile and header file.

--- 42cursus/printf/optimal_prt/ft_printf.h
+++ 42cursus/printf/optimal_prt/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: si-park <si-park@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 21:06:43 by si-park           #+#    #+#             */
-/*   Updated: 2025/01/14 21:39:46 by si-park          ###   ########.fr       */
+/*   Updated: 2025/02/19 12:21:26 by si-park          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,13 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include "libft/libft.h"
 
 int		ft_printf(const char *fmt, ...);
 int		parse_format(const char *fmt, va_list args);
 int		handle_specifier(char c, va_list args);
 int		print_hex(va_list args, char c);
 
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-int		ft_strlen(const char *s);
 int		ft_putnbr_hex(unsigned long num, int is_upper);
 int		ft_print_p(void *p);
 char	*ft_itoa(int n);
