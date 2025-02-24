b369fb7557d993d8a816e9089b6a004a7e66e565
si-park <qkrtlgud95@naver.com>
2025-02-24 19:54:30 +0900

    logic for getnextline

--- 42cursus/getnexline/get_next_line.c
+++ 42cursus/getnexline/get_next_line.c
@@ -6,126 +6,49 @@
 /*   By: si-park <si-park@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 17:27:14 by si-park           #+#    #+#             */
-/*   Updated: 2025/02/23 17:25:33 by si-park          ###   ########.fr       */
+/*   Updated: 2025/02/24 19:53:47 by si-park          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "get_next_line.h"
+// record를 동적할당하고, 문자열을 합치는 함수 추가.
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
-#endif
 
-// 문자열 길이를 구하는 함수
-size_t	ft_strlen(const char *s)
+// 개행을 기준으로 한 줄을 나누는 함수 추가.
+
+
+// record관리 방법을 수정.
+
+
+char *get_next_line(int fd)
 {
-	size_t	len = 0;
-	while (s && s[len])
-		len++;
-	return (len);
+    int reread;
+    //버퍼사이즈만큼의 공간, record는 개행을 기준으로 나눈 문자열을 저장하기 위함.
+    char buf[BUFFER_SIZE + 1];
+    char *record = NULL;
+    
+    //파일 디스크립터는 012를 제외한 3부터의 정수.
+    //read를 통해 파일을 읽고, 반환값을 저장해야함. 반환값이 0이면 파일이 끝났다는 것.
+    //파일이 닫힐 때 까지 계속 while을 돌린다.
+    //reread = read(fd, buf, BUFFER_SIZE);
+    while((reread = read(fd, buf, BUFFER_SIZE)) > 0)
+    {
+        // read가 BUFFER_SIZE만큼 읽은 데이터를 record에 저장한다.
+        // 만약 기존에 record의 값이 존재한다면 데이터를 이어붙인다. static variable
+        
+        //만약 줄내림을 발견한다면, 그 줄내림을 기준으로 앞의 문자열을 반환해야한다.
+        //잘라내고 나머지 데이터는 record에 다시 저장한다. 
+
+
+        if(reread == 0){
+            //파일이 끝났을 때, EOF
+            //record에 남아있는 데이터를 반환하고, NULL로 초기화해야한다.
+        }
+
+        if(reread == -1){
+            //fd가 유효하지 않거나, 허용되지 않은 파일을 읽으려고 할 때, 
+            //record를 free()하고 null을 반환해야함
+        }
+    }
 }
 
-// 문자열을 복사하는 함수
-char	*ft_strdup(const char *s, size_t len)
-{
-	char	*dup = (char *)malloc(len + 1);
-	if (!dup)
-		return (NULL);
-	for (size_t i = 0; i < len; i++)
-		dup[i] = s[i];
-	dup[len] = '\0';
-	return (dup);
-}
-
-// 문자열을 합치는 함수
-char	*ft_strjoin(char *s1, char *s2, size_t len2)
-{
-	size_t	len1 = ft_strlen(s1);
-	char	*joined = (char *)malloc(len1 + len2 + 1);
-	if (!joined)
-		return (NULL);
-	for (size_t i = 0; i < len1; i++)
-		joined[i] = s1[i];
-	for (size_t j = 0; j < len2; j++)
-		joined[len1 + j] = s2[j];
-	joined[len1 + len2] = '\0';
-	free(s1);  // 기존 문자열 해제
-	return (joined);
-}
-
-char	*get_next_line(int fd)
-{
-	static char	*buffer = NULL;
-	char		buf[BUFFER_SIZE + 1];
-	char		*line = NULL;
-	ssize_t		reread;
-	size_t		i;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-
-	// 파일에서 BUFFER_SIZE 만큼 읽기
-	while ((reread = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		buf[reread] = '\0';
-		buffer = ft_strjoin(buffer, buf, reread);
-		if (!buffer)
-			return (NULL);
-
-		// 개행 문자 탐색
-		for (i = 0; buffer[i]; i++)
-		{
-			if (buffer[i] == '\n')
-			{
-				line = ft_strdup(buffer, i + 1);
-				char *temp = ft_strdup(buffer + i + 1, ft_strlen(buffer + i + 1));
-				free(buffer);
-				buffer = temp;
-				return (line);
-			}
-		}
-	}
-
-	// 파일 끝에서 마지막 남은 데이터 반환
-	if (buffer && *buffer)
-	{
-		line = ft_strdup(buffer, ft_strlen(buffer));
-		free(buffer);
-		buffer = NULL;
-		return (line);
-	}
-
-	free(buffer);
-	buffer = NULL;
-	return (NULL);
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc != 2)
-	{
-		printf("Usage: %s <filename>\n", argv[0]);
-		return (1);
-	}
-
-	int fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		perror("open");
-		return (1);
-	}
-
-	char *line;
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("%s", line);
-		free(line);
-	}
-
-	close(fd);
-	return (0);
-}
