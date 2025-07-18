e147c9f9a50a05b364430c6115bbe098a390e036
si-park <qkrtlgud95@naver.com>
2025-07-18 17:51:52 +0900

    asd

--- 42cursus/circle2/push_swap/test.c
+++ 42cursus/circle2/push_swap/test.c
@@ -5,8 +5,23 @@ typedef struct Node{
     struct Node *next;
 } N;
 
+void push_front(N **head, int value){
+    
+}
+
+void print_list(){
+    
+}
+
+
 
 int main(){
+    N *head = NULL;
+
+    push_front(&head, 10);
+    push_front(&head, 20);
+    push_front(&head, 30);
+    print_list(head);  // 출력: 30 20 10
 
     return 0;
 }
