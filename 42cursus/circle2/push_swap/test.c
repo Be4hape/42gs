1ae5cbe8cb6117740dec8a5af4ca2942ff11bdc0
si-park <qkrtlgud95@naver.com>
2025-07-19 10:17:51 +0900

    asd

--- 42cursus/circle2/push_swap/test.c
+++ 42cursus/circle2/push_swap/test.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 typedef struct Node{
     int data;
     struct Node *next;
-} N;
+} Node;
 
-void push_front(N **head, int value){
-    
+
+
+void push_front(Node **head, int value){
+    Node *n = malloc(sizeof(Node));
+
+    n -> data = value;
+    n -> next = *head;
+
+    *head = n;    
 }
 
 void print_list(){
     
 }
 
-
-
 int main(){
-    N *head = NULL;
+    Node *head = NULL;
 
     push_front(&head, 10);
     push_front(&head, 20);
     push_front(&head, 30);
     print_list(head);  // 출력: 30 20 10
+
 
     return 0;
 }
