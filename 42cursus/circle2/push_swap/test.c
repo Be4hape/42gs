29434ccb530a7f6c2da697ec9ec6a6dbc6890ffb
si-park <qkrtlgud95@naver.com>
2025-07-19 18:35:03 +0900

    asd

--- 42cursus/circle2/push_swap/test.c
+++ 42cursus/circle2/push_swap/test.c
@@ -4,7 +4,7 @@
 
 typedef struct Node{
     int data;
-    struct Node *next
+    struct Node *next;
 } Node;
 
 
@@ -13,27 +13,37 @@ void push_front(Node **head, int value){
     Node *n = malloc(sizeof(Node));
     if(!n){
         //malloc 실패
-        
+        perror("malloc fail");
+        exit(EXIT_FAILURE);
     }
-
-
-
     n -> data = value;
     n -> next = *head;
     *head = n;
 }
 
-void print_list(){
-    
+void print_list(Node *head){
+    Node *cur = head;
+    while(cur){
+        printf("%d ", cur -> data);
+        cur = cur -> next;
+    }
+    printf("\n");
 }
 
 
 int main(){
-    Node *head;
-
+    Node *head = NULL;
+    
     push_front(&head, 10);
     push_front(&head, 20);
     push_front(&head, 30);
     print_list(head);  // 출력: 30 20 10
+
+
+    while(head){
+        Node *tmp = head;
+        head = head -> next;
+        free(tmp);
+    }
     return 0;
 }
