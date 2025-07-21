b5726c3cb99c4838b84b43c3de6dfa1cb39908ec
si-park <qkrtlgud95@naver.com>
2025-07-21 18:22:29 +0900

    complete orders, fix bug

--- 42cursus/circle2/push_swap/order.c
+++ 42cursus/circle2/push_swap/order.c
@@ -7,31 +7,17 @@ typedef struct Node {
     struct Node *next;
 } Node;
 
-
-//일반적인 방식으로 연결리스트 data에 value를 넣으려고 하면, 스택처럼 반대로 들어감
-//뒤에서부터 넣어야 함.
-// void push_front(Node **head, int value){
-//     Node *new = malloc(sizeof(Node));
-//     if(!new){
-//         //malloc fail
-//     }
-
-//     new -> data = value;
-//     new -> next = *head;
-//     *head = new;
-// }
-
 void save_head(Node **head, int value){
     Node *new_node = malloc(sizeof(Node));
     if(!new_node){
-        //memory allocation fail case
+        //malloc 실패 시, 프로그램 종료.
+        exit(EXIT_FAILURE);
     }
-        
     new_node -> data = value;
     new_node -> next = NULL;
 
-    //head가 null이라면, 1번만 돌아야 하기 때문에, 바로 NULL을 포함하고 있는
-    //new_node의 값을 *head에 대입한다.
+    //빈 리스트면 꼬리탐색 없이 바로 연결한다. 
+    // 바로 NULL을 포함하고 있는 new_node의 값을 *head에 대입한다.
     if(*head == NULL){
         *head = new_node;
         return ;
@@ -78,53 +64,84 @@ void swap_ss(Node **a, Node **b){
 }
 
 
-
-
 // pa : push a - 스택 b에서 가장 위(탑)에 있는 원소를 가져와서, 
 // 스택 a의 맨 위(탑)에 넣는다. 스택 b가 비어 있으면 아무것도 하지 않는다.
 // pb : push b - 스택 a에서 가장 위(탑)에 있는 원소를 가져와서, 
 // 스택 b의 맨 위(탑)에 넣는다. 스택 a가 비어있으면 아무것도 하지 않는다.
 
 void push(Node **a, Node **b){
-   // -> 이 연산자는 . 연산자와 동일함
-   // *n.next 와 n -> next 는 완전히 동일, 
-   // 포인터 n이 가리키는 Node 구조체 내부의 next 멤버를 뜻함.
-    Node *n = *a;
-    *a = n -> next;
-    n -> next = *b;
+    Node *nA = *a;
+    *a = nA -> next;
 
-    //b로 스왑하여 연결한 노드를, 떼어온 노드 n으로 바꿔줌.
-    *b = n;
+    Node *nB = *b;
+    *b = nA;
+    nA -> next = nB;
 }
 
 void push_pa(Node **a, Node **b){
     //b의 가장 위의 데이터를 a의 가장 위로 옮긴다.
+    push(b, a);
 }
 
 void push_pb(Node **a, Node **b){
     //a의 가장 위의 데이터를 b의 가장 위로 옮긴다.
+    push(a, b);
 }
 
 
 
-
-
-
 // ra : rotate a - 스택 a의 모든 원소들을 위로 1 인덱스만큼 올린다. 
-// 첫 번째 원소(탑)는 마지막 원소(바텀)가 된다.
-
 // rb : rotate b - 스택 b의 모든 원소들을 위로 1 인덱스만큼 올린다. 
-// 첫 번째 원소(탑)는 마지막 원소(바텀)가 된다.
-
 // rr : ra와 rb를 동시에 실행한다.
-
 // rra : reverse rotate a - 스택 a의 모든 원소들을 아래로 1 인덱스만큼 내린다. 
-// 마지막 원소(바텀)는 첫 번째 원소(탑)가 된다.
-
 // rrb : reverse rotate b - 스택 b의 모든 원소들을 아래로 1 인덱스만큼 내린다. 
-// 마지막 원소(바텀)는 첫 번째 원소(탑)가 된다.
-
 // rrr : rra와 rrb를 동시에 실행한다.
+void rotate(Node **head){
+    Node *first = *head;
+    *head = first -> next;
+
+    Node *tail = *head;
+    while(tail -> next)
+        tail = tail -> next;
+    tail = first -> next;
+    first -> next = NULL;
+}
+
+void reverse_rotate(Node **head){
+    Node *prev = NULL;
+    Node *cur = *head;
+    
+    while(cur -> next){
+        prev = cur;
+        cur = cur -> next;
+    }
+    prev -> next = NULL;
+    cur -> next = *head;
+    *head = cur;
+}
+
+
+void rotate_ra(Node **a){
+    rotate(a);
+}
+void rotate_rb(Node **b){
+    rotate(b);
+}
+void rotate_rr(Node **a, Node **b){
+    rotate(a);
+    rotate(b);
+}
+void rotate_rra(Node **a){
+    reverse_rotate(a);
+}
+void rotate_rrb(Node **b){
+    reverse_rotate(b);
+}
+void rotate_rrr(Node **a, Node **b){
+    reverse_rotate(a);
+    reverse_rotate(b);
+}
+
 
 void print_list(Node *head){
     Node *cur = head;
@@ -147,15 +164,22 @@ int main(){
 
     // //swap_sa(&head);
     // print_list(head);
-
     Node *headA = NULL;
     Node *headB = NULL;
 
-    save_head(&headA, 10);
-    save_head(&headA, 20);
-    save_head(&headA, 30);
+    save_head(&headA, 1);
+    save_head(&headA, 2);
+    save_head(&headA, 3);
+    save_head(&headA, 6);
+    save_head(&headA, 5);
+    save_head(&headA, 8);
 
-    swap_sa(&headA);
+    push_pb(&headA, &headB);
+    push_pb(&headA, &headB);
+    push_pb(&headA, &headB);
+
+    rotate_ra(&headA);
+    rotate_rb(&headB);
 
     print_list(headA);
     print_list(headB);
